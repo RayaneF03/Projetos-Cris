43951b8cb6fe313f12f60d04d6b28fc78e9020c1
RayaneF03 <rayane.f.santos7@aluno.senai.br>
2026-02-26 16:37:48 -0300

    Integração EF Core, CRUD de Aluno e documentação Scalar
    Adiciona Entity Framework Core com SQL Server ao projeto, incluindo configuração do AppDbContext, migrations e string de conexão. Implementa a entidade e controller Aluno com endpoints REST para CRUD. Integra Scalar para documentação e testes da API. Ajusta o pipeline no Program.cs para suportar as novas funcionalidades.

---
+++ PrimeiraAPI/Models/Aluno.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrimeiraAPI.Models
+{
+    public class Aluno
+    {
+        public Guid AlunoId { get; set; } // Identificador único do aluno
+        [Required]
+        public string? Nome { get; set; } // Nome do aluno
+        public DateOnly DataNascimento { get; set; } // Data de nascimento do aluno.
+        public int RM { get; set; } // Registro do aluno
+        public bool? CadastroAtivo { get; set; } // Indica se o cadastro do aluno está ativo ou inativo
+        public decimal Mensalidade { get; set; } // Valor da mensalidade do aluno
+        public DateTime? DataCadastro { get; set; } // Data de cadastro do aluno (opcional)
+    }
+}
