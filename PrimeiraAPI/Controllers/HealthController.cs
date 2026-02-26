e5246512976949bdbdcc89309178e8f9236b2531
RayaneF03 <rayane.f.santos7@aluno.senai.br>
2026-02-26 13:45:36 -0300

    Adiciona Scalar.AspNetCore e integra interface de docs
    Inclui o pacote Scalar.AspNetCore para fornecer uma interface alternativa de documentação e testes da API. Atualiza Program.cs para configurar e mapear endpoints do Scalar e OpenAPI (Swagger), além de adicionar comentários explicativos em português sobre o setup e pipeline da aplicação.
    
    Adiciona endpoint de saúde e remove previsão do tempo
    
    Removidos WeatherForecastController e WeatherForecast, eliminando o endpoint de previsão do tempo. Adicionado HealthController com endpoint GET /api/statusAPI para verificar o funcionamento da API.

---
+++ PrimeiraAPI/Controllers/HealthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PrimeiraAPI.Controllers
+{
+    [Route("api/statusAPI")] // rota base: /api/health
+    [ApiController] // ativaar validação automatica
+
+
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            // Retorna um status 200 OK 
+            return Ok("API está funcionando");
+        }
+    }
+}
